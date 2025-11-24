f9156e4a684be758d6fc25c15b09adebf7867a55
kiksknacks <katrin.stecher@campus.lmu.de>
2025-11-24 18:13:57 +0100

    reworked tests according to feedback. no hooks yet.

--- tests/probly/transformation/ensemble/test_common.py
+++ tests/probly/transformation/ensemble/test_common.py
@@ -5,29 +5,23 @@ from __future__ import annotations
 import pytest
 
 from probly.predictor import Predictor
-from tests.probly.fixtures.common import dummy_predictor
 from probly.transformation import ensemble
 from probly.transformation.ensemble.common import *
-       
-def test_base(dummy_predictor: Predictor) -> None:
-    class DummyClass:
-        pass
-    def dummy_generator(base, n_members):
-        return (base, n_members)
-    register(cls = DummyClass, generator = dummy_generator)
 
-def test_ensemble_generator_when_not_registered(dummy_predictor: Predictor) -> None:
-    base = dummy_predictor
-    class DummyClass:
-        pass
-    with pytest.raises(NotImplementedError, match="No ensemble generator is registered for type"):
+class Invalid_Dummy_Predictor(Predictor):
+    def __call__(self, x:int) -> int:
+        return x
+
+class Valid_Dummy_Predictor(Predictor):
+    pass
+
+
+def test_base() -> None:
+    def dummy_generator(base, num_members):
+        return (base, num_members)
+    register(cls = Valid_Dummy_Predictor, generator = dummy_generator)
+
+def test_for_invalid_type() -> None:
+    base = Invalid_Dummy_Predictor()
+    with pytest.raises(NotImplementedError):
         ensemble_generator(base)
-        
-def test_example_ensemble(dummy_predictor: Predictor) -> None:
-    base = dummy_predictor
-    k = 3
-    def dummy_generator(base, n_members):
-        return (base, n_members)
-    register(cls=type(dummy_predictor), generator = dummy_generator)
-    out = ensemble(base, n_members=k)
-    assert out == (base, k)
